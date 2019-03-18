1a48612b0b2900d8f17d2926bc9a1b89cd49fb34
jj888 <jaconjcondes@gmail.com>
2019-03-18 03:26:50 -0700

    Working Subject and To; Work On Message

--- email-client-react-native/screens/ComposeScreen.js
+++ email-client-react-native/screens/ComposeScreen.js
@@ -1,64 +1,51 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-
+import { ScrollView, StyleSheet, View, Text, TextInput } from "react-native";
+// TextInput
 export default class ComposeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      from: "jaconjcondes@gmail.com",
+      to: "",
+      subject: "",
+      message: ""
+    };
+  }
+
   static navigationOptions = {
     title: "Compose Email"
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}
-        >
-          <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-            <Text style={styles.getStartedText}>This Is The Compose</Text>
-          </View>
-        </ScrollView>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>From: {this.state.from}</Text>
+        </View>
+        <View style={styles.toContainer}>
+          <Text style={styles.toTitle}>To: </Text>
+          <TextInput
+            style={styles.toInput}
+            onChangeText={text => this.setState({ to: text })}
+            value={this.state.to}
+          />
+        </View>
+        <View style={styles.subjectContainer}>
+          <Text style={styles.subjectTitle}>Subject: </Text>
+          <TextInput
+            style={styles.subjectInput}
+            onChangeText={text => this.setState({ subject: text })}
+            value={this.state.subject}
+          />
+        </View>
+        <View style={styles.messageContainer}>
+          <TextInput
+            style={styles.messageInput}
+            onChangeText={text => this.setState({ message: text })}
+            value={this.state.message}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -68,17 +55,63 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
-  contentContainer: {
-    paddingTop: 10
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 5
   },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
+  inputText: {
+    fontSize: 21,
+    color: "#2f95dc",
+    lineHeight: 21,
+    textAlign: "left"
   },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
+  toContainer: {
+    padding: 5,
+    flexDirection: "row",
+    borderColor: "green",
+    borderWidth: 1
+  },
+  toTitle: {
+    fontSize: 21,
+    color: "#2f95dc"
+  },
+  toInput: {
+    fontSize: 21,
+    color: "#2f95dc",
+    width: "100%"
+  },
+  subjectContainer: {
+    padding: 5,
+    flexDirection: "row",
+    borderColor: "green",
+    borderWidth: 1
+  },
+  subjectTitle: {
+    fontSize: 21,
+    color: "#2f95dc"
+  },
+  subjectInput: {
+    fontSize: 21,
+    color: "#2f95dc",
+    width: "100%"
+  },
+  messageContainer: {
+    padding: 5,
+    flexDirection: "row",
+    borderColor: "green",
+    borderWidth: 1
+  },
+  messageTitle: {
+    fontSize: 21,
+    color: "#2f95dc"
+  },
+  messageInput: {
+    fontSize: 21,
+    color: "#2f95dc",
+    width: "100%",
+    height: "80%"
   }
 });
